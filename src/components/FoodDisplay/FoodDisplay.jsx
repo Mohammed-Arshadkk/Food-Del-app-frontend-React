f5721bfd028f200787df9ce5ee1ad4168dbe4379
Mohammed Arshad <mohammedarshad62820@gmail.com>
2024-05-06 20:40:50 +0530

    Updated functionality of the cart and display the items

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,10 @@ const FoodDisplay = ({category}) => {
       <h2>Top deashes near you</h2>
       <div className="food-display-list">
         {food_list.map((item,index)=> {
-            return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+          // {console.log(item._id,`this is the id of ${item.name}`)}
+          if(category === '' || category === item.category){
+            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+          } 
         })}
       </div>
     </div>
